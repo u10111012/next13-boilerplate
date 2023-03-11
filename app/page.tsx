6095138b835898ff9506d6382ad6aa2d2fd01a13
LINZHIYU <79639953+u10111012@users.noreply.github.com>
2023-03-11 23:11:05 +0800

    Feat/modify home page (#1)
    * Keep calm and commit
    
    * feat/modify-homePage: husky

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ export default function Home() {
   return (
     <div className="align-middle text-center">
      <h1 className="text-3xl font-bold text-blue-800">Next Boilerplate</h1>
+        <h3 className="text-2xl text-blue-600">This Project is use Next13 with Tailwind</h3>
+        <h3 className="text-2xl text-blue-600">DataFetching is SWR</h3>
       <Link href="/users">
           Users
       </Link>
